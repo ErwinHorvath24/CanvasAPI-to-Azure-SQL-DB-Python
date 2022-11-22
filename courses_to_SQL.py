27a652efac526b557e0d73e25cdcf032fc21a98e
Erwin Horvath <63558249+ehorvath24@users.noreply.github.com>
2022-11-22 16:25:54 -0500

    Update courses_to_SQL.py

--- courses_to_SQL.py
+++ courses_to_SQL.py
@@ -102,7 +102,7 @@ crsr: pyodbc.Cursor = cnxn.cursor()
 
 # SQL Insert
 for index, row in df.iterrows():
-    crsr.execute("INSERT INTO YOUR TABLE NAME HERE (course_id,course_name,account_id,uuid,start_at,grading_standard_id,is_public,created_at,course_code,default_view,root_account_id,enrollment_term_id,license,grade_passback_setting,end_at,public_syllabus,public_syllabus_to_auth,storage_quota_mb,is_public_to_auth_users,homeroom_course,course_color,friendly_name,apply_assignment_group_weights,calendar,time_zone,blueprint,template,sis_course_id,sis_import_id,integration_id,enrollments,hide_final_grades,workflow_state,restrict_enrollments_to_course_dates,overridden_course_visibility,blueprint_restrictions_by_object_type) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+    crsr.execute("INSERT INTO courses (course_id,course_name,account_id,uuid,start_at,grading_standard_id,is_public,created_at,course_code,default_view,root_account_id,enrollment_term_id,license,grade_passback_setting,end_at,public_syllabus,public_syllabus_to_auth,storage_quota_mb,is_public_to_auth_users,homeroom_course,course_color,friendly_name,apply_assignment_group_weights,calendar,time_zone,blueprint,template,sis_course_id,sis_import_id,integration_id,enrollments,hide_final_grades,workflow_state,restrict_enrollments_to_course_dates,overridden_course_visibility,blueprint_restrictions_by_object_type) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
                 row.course_id,
                 row.course_name,
                 row.account_id,
