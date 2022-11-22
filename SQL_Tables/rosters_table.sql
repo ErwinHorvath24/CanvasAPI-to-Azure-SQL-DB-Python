18da78ad45e2ae06cdfbbe1e67ae71a21c09dda9
Erwin Horvath <63558249+ehorvath24@users.noreply.github.com>
2022-11-22 16:36:13 -0500

    Add files via upload

---
+++ SQL_Tables/rosters_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE [roster](
+[student_id] int NOT NULL, 
+[student_name] varchar(500) NOT NULL,
+[created_at] varchar(500) NOT NULL, 
+[sortable_name] varchar(500) NOT NULL, 
+[short_name] varchar(500) NOT NULL, 
+[sis_user_id] varchar(500) NOT NULL, 
+[integration_id] varchar(500) NOT NULL, 
+[sis_import_id] int NOT NULL, 
+[login_id] varchar(500) NOT NULL,
+[course_id] int NOT NULL 
+)
+GO
